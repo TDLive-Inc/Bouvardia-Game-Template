ca83f3e416ddae0b7c3ddce89060f58da3ae2b2f
TDLive <thunderlive-sunshine@live.com>
2013-06-02 17:29:22 -0300

    fix javascript errors and remove some newlines for visibility

--- game.js
+++ game.js
@@ -4,18 +4,15 @@ function alreadyIncluded(){
 
 function bootstrap(Bouvardiaversion){
 	
-	game_information = [];
-	
+	game_information = []; //don't delete this line!!	
 	game_information["fancy_introduction"] = true;
 	game_information["name"] = "Bouvardia Game Template"
 	game_information["publisher"] = "Bouvardia Team"
 	game_information["copyright"] = "Copyright TDLive.org Incorporated"
-	game_information["version"] = "1.0"
-	
+	game_information["version"] = "1.0"	
 	game_information["introduction"] = "This is a game template for Bouvardia."
 	
-	room_zero = [];
-	
+	room_0 = []; //room 0
 	room_0["name"] = "Room Zero";
 	room_0["description"] = "This. Is. Room. Zero.";
 	room_0["connectsWest"] = undefined;
@@ -23,12 +20,10 @@ function bootstrap(Bouvardiaversion){
 	room_0["connectsNorth"] = undefined;
 	room_0["connectsSouth"] = undefined;
 	room_0["item"] = 0;
-	room_0["requiredItem"] = undefined;
-	
+	room_0["requiredItem"] = undefined;	
 	addRoom(room_zero);
 	
-	room_1 = []
-	
+	room_1 = []; //room 1
 	room_1["name"] = "Room One";
 	room_1["description"] = "This. Is. Room. One.";
 	room_1["connectsWest"] = 0;
@@ -36,13 +31,12 @@ function bootstrap(Bouvardiaversion){
 	room_1["connectsNorth"] = undefined;
 	room_1["connectsSouth"] = undefined;
 	room_1["item"] = undefined;
-	room_1["requiredItem"] = 0;
-	
+	room_1["requiredItem"] = 0;	
 	addRoom(room_1);
 	
 	item_0 = []
 	item_0["name"] = "Room One Key";
-	
 	addItem(item_0);
+
 	return true;
 }
